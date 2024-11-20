0b27a53751d1c3fc9ba5036ffbca6837c3bfe734
WUKE7 <kevin.wu@oocl.com>
2024-11-20 14:42:08 +0800

    refactor: rename inputList as frequencies

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,23 +8,23 @@ public class WordFrequencyGame {
             try {
                 //split the input string with 1 to n pieces of spaces
                 String[] words = sentence.split("\\s+");
-                List<Input> inputList = new ArrayList<>();
+                List<Input> frequencies = new ArrayList<>();
                 for (String word : words) {
                     Input input = new Input(word, 1);
-                    inputList.add(input);
+                    frequencies.add(input);
                 }
                 //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
+                Map<String, List<Input>> map = getListMap(frequencies);
                 List<Input> list = new ArrayList<>();
                 for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
                     Input input = new Input(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
-                inputList = list;
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                frequencies = list;
+                frequencies.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
+                for (Input w : frequencies) {
                     String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
                 }
