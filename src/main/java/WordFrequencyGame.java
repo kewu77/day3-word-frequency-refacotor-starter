a108a8468930a72aacffb20bafbe1890c178760f
WUKE7 <kevin.wu@oocl.com>
2024-11-20 14:14:30 +0800

    refactor: remove useless empty row

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,33 +2,25 @@ import java.util.*;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
-
-
         if (inputStr.split("\\s+").length == 1) {
             return inputStr + " 1";
         } else {
-
             try {
-
                 //split the input string with 1 to n pieces of spaces
                 String[] arr = inputStr.split("\\s+");
-
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
                     Input input = new Input(s, 1);
                     inputList.add(input);
                 }
-
                 //get the map for the next step of sizing the same word
                 Map<String, List<Input>> map = getListMap(inputList);
-
                 List<Input> list = new ArrayList<>();
                 for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
                     Input input = new Input(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
                 inputList = list;
-
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
@@ -38,8 +30,6 @@ public class WordFrequencyGame {
                 }
                 return joiner.toString();
             } catch (Exception e) {
-
-
                 return "Calculate Error";
             }
         }
@@ -58,8 +48,6 @@ public class WordFrequencyGame {
                 map.get(input.getValue()).add(input);
             }
         }
-
-
         return map;
     }
 
