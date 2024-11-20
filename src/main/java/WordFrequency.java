27753526c9db4a00a38aada7419801a150b85f8c
WUKE7 <kevin.wu@oocl.com>
2024-11-20 14:46:41 +0800

    refactor: rename i as count

--- src/main/java/WordFrequency.java
+++ src/main/java/WordFrequency.java
@@ -2,9 +2,9 @@ public class WordFrequency {
     private String word;
     private int count;
 
-    public WordFrequency(String word, int i){
+    public WordFrequency(String word, int count){
         this.word =word;
-        this.count =i;
+        this.count =count;
     }
 
 
